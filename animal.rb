410ce66a66f42fcc96339445775cf3b48fdbdf21
lucascomamala <lucascomamala@gmail.com>
2023-04-13 00:06:46 -0600

    Abstraction example

--- animal.rb
+++ animal.rb
@@ -11,17 +11,16 @@ class Animal
   end
 
   def speak
-    "Bla bla bla"
+    if @type == "dog"
+      "Woof, woof"
+    elsif @type == "spider"
+      "..."
+    end
   end
 end
 
 animal_1 = Animal.new("dog", 4, "Rex")
-p animal_1.id
-p animal_1.type
-p animal_1.name
-p animal_1.number_of_legs
+animal_2 = Animal.new("spider", 8, "Wilma")
 
-p animal_2 = Animal.new("cat", 8)
-p animal_2.name
-p animal_2.name = "Fluffy"
-p animal_2.name
+p animal_1.speak()
+p animal_2.speak()
