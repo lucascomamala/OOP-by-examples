9f684d279d6c1a083bf7842d4941bc9ac5ad08bb
lucascomamala <lucascomamala@gmail.com>
2023-04-13 00:04:54 -0600

    Encapsulation example

--- animal.rb
+++ animal.rb
@@ -1,8 +1,13 @@
 class Animal
-  def initialize(number_of_legs, name = "Unknown")
+  attr_reader :id, :number_of_legs, :type
+  attr_accessor :name
+
+
+  def initialize(type, number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
+    @type = type
   end
 
   def speak
@@ -10,11 +15,13 @@ class Animal
   end
 end
 
-animal_1 = Animal.new(4, "Rex")
-animal_2 = Animal.new(8)
+animal_1 = Animal.new("dog", 4, "Rex")
+p animal_1.id
+p animal_1.type
+p animal_1.name
+p animal_1.number_of_legs
 
-p animal_1
-p animal_2
-
-p animal_1.speak
-p animal_2.speak
+p animal_2 = Animal.new("cat", 8)
+p animal_2.name
+p animal_2.name = "Fluffy"
+p animal_2.name
